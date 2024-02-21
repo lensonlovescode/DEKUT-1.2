0079f99a9cd4e596c91e1c29ed0af7f12df316a4
Lenson Mutugi <125955780+lensonlovescode@users.noreply.github.com>
2024-02-21 09:14:09 +0300

    directories refresh

---
+++ iteration/operators.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int result, num1, num2;
+    char op;
+
+
+    printf("Enter the first number: \n");
+    scanf("%d", &num1);
+    printf("Enter the second number: \n");
+    scanf("%d", &num2);
+    printf("Enter operator: \n");
+    scanf("%c", &op);
+
+    switch (op)
+    {
+    case '+':
+    {
+        result = num1 + num2;
+        printf("%d + %d = %d", num1, num2, result);
+        break;
+    }
+    case '-':
+    {
+        result = num1 - num2;
+        printf("%d - %d = %d", num1, num2, result);
+    break;
+    }
+    case '*':
+    {
+        result = num1 * num2;
+        printf("%d * %d = %d", num1, num2, result);
+    break;
+    }
+    case '/':
+    {
+        result = num1 / num2;
+        printf("%d / %d = %d", num1, num2, result);
+    break;
+    }
+    case '%':
+    {
+        result = num1 % num2;
+        printf("%d %% %d = %d", num1, num2, result);
+    break;
+    }
+    default:{
+        printf("Wrong opeator");
+    break;
+    }
+    }
+    return (0);
+}
